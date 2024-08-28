04f33dfd5976b77b91d943af653e40546076e9e4
MichaelReeves2 <tadeas.jindra28@seznam.cz>
2024-08-28 20:10:48 +0200

    wd

--- storage/get_temp.php
+++ storage/get_temp.php
@@ -1,9 +1,8 @@
 <?php
-// Read CPU temperature from the file
 $temperatureFile = '/sys/class/thermal/thermal_zone0/temp';
 if (file_exists($temperatureFile)) {
     $temp = file_get_contents($temperatureFile);
-    $tempCelsius = round($temp / 1000, 1); // Convert temperature to Celsius
+    $tempCelsius = round($temp / 1000, 1);
     echo $tempCelsius;
 } else {
     echo "Error: Temperature file not found.";
